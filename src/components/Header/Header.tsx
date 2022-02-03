e6ca0eb8c19cf09762e31e6175552160f1f4d814
Jonah Lin <57741810+linjonah@users.noreply.github.com>
2022-02-03 12:56:35 -0800

    basic header component

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import styled from "styled-components";
+
+const Container = styled.div`
+  display: block;
+  width: 100%;
+`
+
+export const Header = () => {
+  return (
+    <Container>
+      Header
+    </Container>
+  )
+};
---
+++ src/pages/App.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import styled from 'styled-components';
+import { ConnectWallet } from '../components/ConnectWallet/ConnectWallet';
+import { Header } from '../components/Header/Header';
+
+const Wrapper = styled.div`
+  background: rgb(11, 15, 28);
+  height: 100%;
+`
+
+function App() {
+  return (
+    <Wrapper>
+      <Header />
+      <ConnectWallet />
+    </Wrapper>
+  );
+}
+
+export default App;
