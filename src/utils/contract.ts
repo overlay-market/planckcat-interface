521a10082dc75bcc58e1885c14e3e8e380fde34b
Jonah Lin <57741810+linjonah@users.noreply.github.com>
2022-02-15 17:14:25 -0800

    utils contract

---
+++ src/utils/contract.ts
@@ -0,0 +1,38 @@
+import { Contract } from "@ethersproject/contracts";
+import { AddressZero } from "@ethersproject/constants";
+import { JsonRpcSigner, Web3Provider } from "@ethersproject/providers";
+import { isAddress } from "./web3";
+
+// account is not optional
+export function getSigner(
+  library: Web3Provider,
+  account: string
+): JsonRpcSigner {
+  return library.getSigner(account).connectUnchecked();
+}
+
+// account is optional
+export function getProviderOrSigner(
+  library: Web3Provider,
+  account?: string
+): Web3Provider | JsonRpcSigner {
+  return account ? getSigner(library, account) : library;
+}
+
+// account is optional
+export function getContract(
+  address: string,
+  ABI: any,
+  library: Web3Provider,
+  account?: string
+): Contract {
+  if (!isAddress(address) || address === AddressZero) {
+    throw Error(`Invalid 'address' parameter '${address}'.`);
+  }
+
+  return new Contract(
+    address,
+    ABI,
+    getProviderOrSigner(library, account) as any
+  );
+}
