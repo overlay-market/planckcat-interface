8b8abf5d17b3545bd68fd500482a516232071ad4
Jonah Lin <57741810+linjonah@users.noreply.github.com>
2022-03-25 12:06:40 -0700

    graphQlRequestBaseQuery

--- src/state/data/slice.ts
+++ src/state/data/slice.ts
@@ -12,3 +12,41 @@ const CHAIN_SUBGRAPH_URL: Record<number, string> = {
   [SupportedChainId.ARBITRUM]:
     "https://api.thegraph.com/subgraphs/name/bigboydiamonds/planckcats-subgraph",
 };
+
+// Graphql query client wrapper that builds a dynamic url based on chain id
+function graphqlRequestBaseQuery(): BaseQueryFn<
+  { document: string | DocumentNode; variables?: any },
+  unknown,
+  Pick<ClientError, "name" | "message" | "stack">,
+  Partial<Pick<ClientError, "request" | "response">>
+> {
+  return async ({ document, variables }, { getState }: BaseQueryApi) => {
+    try {
+      const chainId = (getState() as AppState).application.chainId;
+
+      // if chainId in state is null, set default query to Mainnet
+      const subgraphUrl = chainId ? CHAIN_SUBGRAPH_URL[chainId] : CHAIN_SUBGRAPH_URL[1];
+
+      if (!subgraphUrl) {
+        return {
+          error: {
+            name: "UnsupportedChainId",
+            message: `Subgraph queries against ChainId ${chainId} are not supported.`,
+            stack: "",
+          },
+        };
+      }
+
+      return {
+        data: await new GraphQLClient(subgraphUrl).request(document, variables),
+        meta: {},
+      };
+    } catch (error) {
+      if (error instanceof ClientError) {
+        const { name, message, stack, request, response } = error;
+        return { error: { name, message, stack }, meta: { request, response } };
+      }
+      throw error;
+    }
+  };
+}
