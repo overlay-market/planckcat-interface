79cf8ed2cc0f3524b546e59f7ccf0cfb0365ae2a
Jonah Lin <57741810+linjonah@users.noreply.github.com>
2022-03-28 14:51:38 -0700

    show claimable token amount and claim button if useCanClaim returns array with length greater than 0

--- src/hooks/useCanClaim.ts
+++ src/hooks/useCanClaim.ts
@@ -4,7 +4,6 @@ import PlanckCatMinter_ABI from '../constants/abis/PlanckCatMinter.json';
 
 export function useCanClaim(accountAddress?: string): any {
   const [{ data: signerData, error: signerError, loading: signerLoading }, getSigner] = useSigner();
-  const [{ data: blockData, error: blockError, loading: blockLoading }, getBlockNumber] = useBlockNumber()
   const contract = useContract({
     addressOrName: '0x2A2e181Cc177974c5D013240C34E1dEf1A3CC31a',
     contractInterface: PlanckCatMinter_ABI,
--- src/state/pages/Claim.tsx
+++ src/state/pages/Claim.tsx
@@ -2,7 +2,7 @@
 import { useState, useEffect } from "react";
 import styled from "styled-components";
 import { TEXT } from "../../theme/theme";
-import { useContract, useSigner, useAccount } from "wagmi";
+import { useContract, useSigner, useAccount, useContractWrite } from "wagmi";
 import PlanckCatMinter_ABI from '../../constants/abis/PlanckCatMinter.json';
 import { useCanClaim } from "../../hooks/useCanClaim";
 
@@ -14,10 +14,25 @@ const Container = styled.div`
   flex: 1;
 `;
 
+const ClaimButton = styled.button`
+  background: #56CCF2; 
+  border: 2px solid #56CCF2;
+  border-radius: 8px;
+  color: #f2f2f2;
+  font-size: 12px;
+  cursor: pointer;
+  padding: 12px;
+  width: 200px;
+  font-family: 'Press Start 2P';
+  box-shadow: 0 0 5px #56ccf2;
+  margin: 24px auto auto auto;
+`;
+
 export function Claim() {
   const [{ data: accountData }] = useAccount();
   const claimable = useCanClaim(accountData?.address);
 
+  // const claimable = [1,3]
   return (
     <Container>
       {claimable == null && (
@@ -25,10 +40,20 @@ export function Claim() {
           Checking for claimable tokens...
         </TEXT.StandardBody>
       )}
-      {claimable.length == 0 && (
+      {claimable.length === 0 && (
         <TEXT.StandardBody m={'auto'}>
           You have no claimable tokens currently.
         </TEXT.StandardBody>
+      )}
+      {claimable.length > 0 && (
+        <>
+          <TEXT.StandardBody m={'auto auto 0 auto'}>
+            You have {claimable.length} tokens available to claim.
+          </TEXT.StandardBody>
+          <ClaimButton>
+            Claim
+          </ClaimButton>
+        </>
       )}
     </Container>
   )
