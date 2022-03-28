933c413477738a2c1f89828bf283e1ffb0842e30
Jonah Lin <57741810+linjonah@users.noreply.github.com>
2022-03-28 15:18:11 -0700

    Inventory page to show default msg if no tokens held in connected wallet

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
 import { Route, Switch, Redirect } from 'react-router-dom';
-import { ConnectWallet } from './components/ConnectWallet/ConnectWallet';
 import { Header } from './components/Header/Header';
-import { TEXT } from './theme/theme';
-import { useAccount, useConnect, useContract, useContractRead, useNetwork, useProvider, useSigner } from 'wagmi';
-import PlanckCat from './assets/planck-cat.png';
-import { ShellTerminal } from './components/ShellTerminal/ShellTerminal';
-import { useWalletTokens } from './state/wallet/hooks';
-import PlanckCatMinter_ABI from './constants/abis/PlanckCatMinter.json';
 import { Claim } from './state/pages/Claim';
 import { Home } from './state/pages/Home';
 import { Inventory } from './state/pages/Inventory';
--- src/state/pages/Inventory.tsx
+++ src/state/pages/Inventory.tsx
@@ -1,20 +1,45 @@
 import styled from "styled-components"
 import { TEXT } from "../../theme/theme";
+import { useWalletTokens } from "../wallet/hooks";
+import { useAccount } from "wagmi";
 
 const Container = styled.div`
   display: flex;
-  flex-direction: column;
+  flex-direction: row;
+  flex-wrap: wrap;
   height: 500px;
+  max-width: 1000px;
   margin: 50px auto auto auto;
   flex: 1;
 `;
 
+const TokenCard = styled.div`
+  height: 200px;
+  width: 200px;
+  text-align: center;
+  border: 1px solid white;
+  color: white;
+`;
+
 export function Inventory() {
+  const [{ data: accountData }] = useAccount();
+  const { tokens } = useWalletTokens(accountData?.address);
+
+  console.log('tokens: ' , tokens);
   return (
     <Container>
-      <TEXT.StandardBody m={'auto'}>
-        Inventory Page
-      </TEXT.StandardBody>
+      {accountData && tokens && (
+        tokens.map((token) => (  
+          <TokenCard>
+            {token.id}
+          </TokenCard>
+        ))
+      )}
+      {!tokens && (
+        <TEXT.StandardBody m={'auto'}>
+          Wallet does not hold any tokens.
+        </TEXT.StandardBody>
+      )}
     </Container>
   )
 }
