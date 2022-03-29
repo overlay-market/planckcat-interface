a221067f2e76aeff18c3ce5a368e0c9a7395d78c
Jonah Lin <57741810+linjonah@users.noreply.github.com>
2022-03-29 11:28:51 -0700

    underline navigation link when on route

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -4,6 +4,7 @@ import ConnectWalletModal from "../ConnectWallet/ConnectWalletModal";
 import { ConnectWallet } from "../ConnectWallet/ConnectWallet";
 import { TEXT } from "../../theme/theme";
 import { StyledInternalLink } from "../../theme/components";
+import { NavLink } from "react-router-dom";
 
 const Container = styled.div`
   display: block;
@@ -28,6 +29,28 @@ const MenuLink = styled(NavigationLink)`
   font-size: 12px;
 `
 
+const activeClassName = 'ACTIVE'
+
+const StyledNavLink = styled(NavLink).attrs({
+  activeClassName,
+})`
+  border-radius: 3rem;
+  outline: none;
+  cursor: pointer;
+  text-decoration: none;
+  color: ${({ theme }) => theme.text1};
+  font-size: 12px;
+  font-weight: 500;
+  padding: 8px 16px;
+  word-break: break-word;
+  overflow: hidden;
+  white-space: nowrap;
+  &.${activeClassName} {
+    text-decoration: underline;
+    text-underline-offset: 3px;
+  }
+`
+
 export const Header = () => {
   return (
     <Container>
@@ -36,13 +59,13 @@ export const Header = () => {
           PlanckCatDAO
         </NavigationLink>
 
-        <MenuLink to={'/Claim'}>
+        <StyledNavLink to={'/Claim'}>
           Claim
-        </MenuLink>
+        </StyledNavLink>
 
-        <MenuLink to={'/Inventory'}>
+        <StyledNavLink to={'/Inventory'}>
           Inventory
-        </MenuLink>
+        </StyledNavLink>
         <ConnectWallet />
       </FlexRowContainer>
 
